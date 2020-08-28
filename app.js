db49be73329900beb6eca60f6553826b8e70f79b
danil milostiviy <milostiviy.danil@yandex.ru>
2020-08-28 15:34:43 +0300

    add POST req

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = express();
+const {v4} = require('uuid');
 const path = require('path');
 
 const CONTACTS = [
-    {id: 1, name: 'Danil', value: '8-800-555-55-55', marked: false}
+    {id: v4(), name: 'Danil', value: '8-800-555-55-55', marked: false}
 ];
+
+app.use(express.json());
 
 app.get('/api/contacts', (req, res) => {
     res.status(200).json(CONTACTS);
+})
+
+app.post('/api/contacts', (req, res) => {
+    const contact = {...req.body, id: v4(), marked: false};
+    CONTACTS.push(contact);
+    res.status(201).json(contact);
 })
 
 app.use(express.static(path.resolve(__dirname, 'client')));
