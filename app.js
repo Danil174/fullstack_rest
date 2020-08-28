41c86048f611ee1182f4790f73a9176e101b69e6
danil milostiviy <milostiviy.danil@yandex.ru>
2020-08-28 16:07:05 +0300

    add PUT and DELETE reqts

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const {v4} = require('uuid');
 const path = require('path');
 
-const CONTACTS = [
+let CONTACTS = [
     {id: v4(), name: 'Danil', value: '8-800-555-55-55', marked: false}
 ];
 
@@ -11,13 +11,24 @@ app.use(express.json());
 
 app.get('/api/contacts', (req, res) => {
     res.status(200).json(CONTACTS);
-})
+});
 
 app.post('/api/contacts', (req, res) => {
     const contact = {...req.body, id: v4(), marked: false};
     CONTACTS.push(contact);
     res.status(201).json(contact);
-})
+});
+
+app.delete('/api/contacts/:id', (req, res) => {
+    CONTACTS = CONTACTS.filter(it => it.id !== req.params.id);
+    res.status(200).json({message: 'succes'});
+});
+
+app.put('/api/contacts/:id', (req, res) => {
+    const idx = CONTACTS.findIndex(it => it.id === req.params.id);
+    CONTACTS[idx] = req.body;
+    res.json(CONTACTS[idx]);
+});
 
 app.use(express.static(path.resolve(__dirname, 'client')));
 
